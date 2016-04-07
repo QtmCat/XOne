a290b250ed4191ef8745239b1546f0c9433cee7b
shenglei <794808060@qq.com>
2016-04-07 21:29:59 +0800

    增加MainMenu界面

---
+++ Code/XOne/Assets/Script/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using QtmCat;
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        AUIManager.OpenDialog("Prefab/UI/MainMenu");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
