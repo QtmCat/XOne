c3b839499df34323f93f39af404e741cc2dbe8d9
shenglei <794808060@qq.com>
2016-04-07 22:57:59 +0800

    修改mainmenu

---
+++ Code/XOne/Assets/Script/UI/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using QtmCat;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.LogError("1234569");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartButtonClick()
+    {
+        Debug.LogError("1234569");
+        ADebug.Log("123");
+    }
+}
