6dab654290d930aa0ac597c63f5dd21f5bafd82d
scottcgi <scottcgi@gmail.com>
2016-04-12 23:43:40 +0800

    添加状态机代码

---
+++ Code/XOne/Assets/Game/Script/Tool/StateMachine.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace QtmCatFramework
+{
+	public class StateMachine
+	{
+		private Dictionary<int, State> stateDic = new Dictionary<int, State>();
+		private State                  curState;
+		private State                  preStae;
+
+
+		public void AddState(State state)
+		{
+			// stateDic.Add(state.id, state);
+		}
+
+
+
+		public class State
+		{
+			private int    id;
+			private object userData;
+			private Action OnEnter;
+			private Action OnExit ;
+			private Action Update ;
+
+			public State SetId(int id)
+			{
+				this.id = id;
+				return this;
+			}
+
+			public State SetUserData(object userData)
+			{
+				this.userData = userData;
+				return this;
+			}
+
+			public State SetOnEnter(Action OnEnter)
+			{
+				this.OnExit = OnEnter;
+				return this;
+			}
+
+			public State SetOnExit(Action OnExit)
+			{
+				this.OnExit = OnExit;
+				return this;
+			}
+
+			public State SetUpdate(Action Update)
+			{
+				this.Update = Update;
+				return this;
+			}
+		}
+	}
+}
+
